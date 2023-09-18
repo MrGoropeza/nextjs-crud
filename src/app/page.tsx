99637692b3345b1c7335e04404b3419e31aad802
MrGoropeza <goropeza8@gmail.com>
2023-09-18 12:45:37 -0300

    feat: project initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "primereact/button";
+
+export default function Home() {
+  return (
+    <main className="p-20">
+      <Button label="Hola" />
+    </main>
+  );
+}
